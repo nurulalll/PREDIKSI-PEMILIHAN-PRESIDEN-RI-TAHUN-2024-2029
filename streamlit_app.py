d3b45336a4b23b10fae87263ee5fb36cf4a6055a
Nurul Aini Lativah <124959830+nurulalll@users.noreply.github.com>
2024-05-20 20:57:23 +0700

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,3 @@
-git clone https://github.com/plotly/plotly.py.git
-cd plotly.py
-pip install .
-
 import streamlit as st
 import pandas as pd
 import plotly.express as px
@@ -14,89 +10,94 @@ def load_data(dataset_name):
     df = pd.read_excel(dataset_name)
     return df
 
-def sidebar():
-    st.sidebar.title('Prediksi Peilihan Presiden RI tahun 2024-2029')
-    # Tambahkan ikon pada sidebar
-    st.sidebar.image('https://png.pngtree.com/png-clipart/20230515/original/pngtree-election-icon-indonesian-symbol-png-image_9161556.png', width=200)
-    st.sidebar.title('Menu')
-    page = st.sidebar.radio("Navigate", ["Dashboard", "Text Sentiment"])
+def header():
+    st.markdown(
+        """
+        <style>
+        .center {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+    st.markdown('<div class="center"><h1>Prediksi Pilihan Presiden RI tahun 2024-2029</h1></div>', unsafe_allow_html=True)
+    st.image('https://img.okezone.com/content/2018/02/19/337/1861446/sejarah-pemilu-dari-masa-ke-masa-cyx7Flt69A.jpg', width=400)
+
+def display_wordcloud(df):
+    st.title("Word Cloud")
+    tweet_text = ' '.join(df['Tweet'].astype(str).tolist())
+    wordcloud = WordCloud(width=800, height=550, background_color ='white').generate(tweet_text)
+    plt.figure(figsize=(12, 8))
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis("off")
+    st.pyplot(plt, clear_figure=False)
+
+def display_sentiment_distribution(df):
+    st.subheader("Sentiment Distribution")
+    sentiment_counts = df['sentimen'].value_counts()
+    fig = px.pie(sentiment_counts, values=sentiment_counts.values, names=sentiment_counts.index)
+    fig.update_layout(width=600, height=400)
+    st.plotly_chart(fig)
+
+def display_top_usernames(df):
+    st.title("Top Usernames")
+    top_usernames = df['username'].value_counts().head(10)
+    fig = px.bar(top_usernames, x=top_usernames.index, y=top_usernames.values,
+                 labels={'x':'Username', 'y':'Count'})
+    fig.update_layout(width=800, height=400, xaxis={'categoryorder':'total descending'})
+    st.plotly_chart(fig)
+
+def text_sentiment():
+    st.title('Analisis Text Sentiment')
+    sentiment_analysis = pipeline("sentiment-analysis")
+    input_text = st.text_area("Masukkan kalimat yang ingin di  analisis:")
+    button = st.button("Analisis")
+
+    if button:
+        with st.spinner("Sedang menganalisis..."):
+            result = sentiment_analysis(input_text)[0]
+        # Menentukan warna teks berdasarkan sentimen
+        sentiment_color = "green" if result['label'] == 'POSITIVE' else "red" if result['label'] == 'NEGATIVE' else "black"
+        # Menampilkan hasil analisis dengan gaya teks yang menarik
+        st.write(f"**Sentimen:** <span style='color:{sentiment_color}; font-weight:bold;'>{result['label']}</span>", 
+                 f"**Score:** {result['score']:.2f}", 
+                 unsafe_allow_html=True)
+
+def main():
+    st.set_page_config(page_title='Sentiment Analysis Dashboard')
+
+    header()
+
     dataset_names = {
         "Anies-CakImin": "Dataset_Anies-CakImin.xlsx",
         "Prabowo-Gibran": "Dataset_Prabowo-Gibran.xlsx",
         "Ganjar-Mahfud": "Dataset_Ganjar-Mahfud.xlsx"
     }
-    selected_dataset = st.sidebar.selectbox("Select Dataset", list(dataset_names.keys()))
-    df = load_data(dataset_names[selected_dataset])
-    return page, df
 
-def dashboard(df):
-    st.title('Dashboard Sentimen Analisis Twitter')
+    selected_datasets = st.multiselect("Select Datasets", list(dataset_names.keys()))
 
-    # Display dataset information
-    col2, col3 = st.columns(2)
-    with col2:
-        st.metric("Number of rows", df.shape[0])
-    with col3:
-        st.metric("Number of columns", df.shape[1])
+    page = st.radio("Navigate", ["Visualisasi", "Text Sentiment"])
 
-    # Display pie chart and word cloud side by side
-    col1, col2 = st.columns(2)
+    dfs = [load_data(dataset_names[dataset]) for dataset in selected_datasets]
+    df = pd.concat(dfs) if dfs else None
 
-        # Visualize sentiment using pie chart
-    with col1:
-        st.subheader("Sentiment Distribution")
-        sentiment_counts = df['sentimen'].value_counts()
-        fig = px.pie(sentiment_counts, values=sentiment_counts.values, names=sentiment_counts.index)
-        fig.update_layout(width=300, height=290)  # Mengatur ukuran pie chart
-        st.plotly_chart(fig)
+    if page == 'Visualisasi':
+        if df is not None:
+            visualization_options = st.multiselect("Choose Visualizations", ["Word Cloud", "Sentiment Distribution", "Top Usernames"])
+            if "Word Cloud" in visualization_options:
+                display_wordcloud(df)
+            if "Sentiment Distribution" in visualization_options:
+                display_sentiment_distribution(df)
+            if "Top Usernames" in visualization_options:
+                display_top_usernames(df)
 
-
-    # Create wordcloud
-    with col2:
-        st.subheader("Word Cloud")
-        tweet_text = ' '.join(df['Tweet'].astype(str).tolist())
-        wordcloud = WordCloud(width=800, height=550, background_color ='white').generate(tweet_text)
-        plt.figure(figsize=(12, 12))
-        plt.imshow(wordcloud, interpolation='bilinear')
-        plt.axis("off")
-        st.pyplot(plt, clear_figure=False)
-
-    # Display bar chart for top usernames
-    st.subheader("Top Usernames")
-    top_usernames = df['username'].value_counts().head(10)
-    fig = px.bar(top_usernames, x=top_usernames.index, y=top_usernames.values,
-                 labels={'x':'Username', 'y':'Count'})
-    fig.update_layout(xaxis={'categoryorder':'total descending'})  # Mengurutkan username berdasarkan jumlah tweet
-    st.plotly_chart(fig)
-
-
-
-def text_sentiment():
-    st.title('Text Sentiment')
-    sentiment_analysis = pipeline("sentiment-analysis")
-    input_text = st.text_area("Masukkan kalimat:")
-    button = st.button("Analysis")
-
-    if button:
-        with st.spinner("Sedang menganalisis..."):
-            result = sentiment_analysis(input_text)[0]
-        st.write(f"Kalimat yang dimasukkan: {input_text}")
-        st.write(f"Sentimen: {result['label']}")
-        st.write(f"Confidence Score: {result['score']}")
-    else:
-        st.write("Ketikkan kalimat di atas dan klik tombol 'Analysis' untuk menganalisis sentimen.")
-
-
-def main():
-    st.set_page_config(page_title='Sentiment Analysis Dashboard')
-
-    page, df = sidebar()
-    
-    if page == 'Dashboard':
-        dashboard(df)
     elif page == 'Text Sentiment':
         text_sentiment()
 
 if __name__ == "__main__":
     main()
-
